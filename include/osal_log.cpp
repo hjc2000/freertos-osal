e1f13ab2583404e4ea58da686ef58390036d05ed
hjc2000 <hjc.666@foxmail.com>
2025-03-18 16:27:43 +0800

    di

--- include/osal_log.cpp
+++ include/osal_log.cpp
@@ -24,63 +24,63 @@
 
 namespace
 {
-    char _buffer[1024];
+	char _buffer[1024];
 
-    class MutexProvider
-    {
-    public:
-        std::shared_ptr<base::IMutex> _lock = base::di::CreateIMutex();
-    };
+	class MutexProvider
+	{
+	public:
+		std::shared_ptr<base::IMutex> _lock = base::CreateIMutex();
+	};
 
-    class Getter :
-        public bsp::TaskSingletonGetter<MutexProvider>
-    {
-    public:
-        std::unique_ptr<MutexProvider> Create() override
-        {
-            return std::unique_ptr<MutexProvider>{new MutexProvider{}};
-        }
-    };
+	class Getter :
+		public bsp::TaskSingletonGetter<MutexProvider>
+	{
+	public:
+		std::unique_ptr<MutexProvider> Create() override
+		{
+			return std::unique_ptr<MutexProvider>{new MutexProvider{}};
+		}
+	};
 } // namespace
 
 void os_log(uint8_t type, char const *fmt, ...)
 {
-    Getter g;
-    base::LockGuard lg{*g.Instance()._lock};
+	Getter g;
+	base::LockGuard lg{*g.Instance()._lock};
 
-    // 初始化缓冲区
-    _buffer[0] = '\0';
+	// 初始化缓冲区
+	_buffer[0] = '\0';
 
-    // 根据日志级别添加前缀
-    switch (LOG_LEVEL_GET(type))
-    {
-    case LOG_LEVEL_DEBUG:
-        snprintf(_buffer, sizeof(_buffer), "%10ld [DEBUG] ", xTaskGetTickCount());
-        break;
-    case LOG_LEVEL_INFO:
-        snprintf(_buffer, sizeof(_buffer), "%10ld [INFO ] ", xTaskGetTickCount());
-        break;
-    case LOG_LEVEL_WARNING:
-        snprintf(_buffer, sizeof(_buffer), "%10ld [WARN ] ", xTaskGetTickCount());
-        break;
-    case LOG_LEVEL_ERROR:
-        snprintf(_buffer, sizeof(_buffer), "%10ld [ERROR] ", xTaskGetTickCount());
-        break;
-    case LOG_LEVEL_FATAL:
-        snprintf(_buffer, sizeof(_buffer), "%10ld [FATAL] ", xTaskGetTickCount());
-        break;
-    default:
-        return; // 如果是未知的日志级别，直接返回
-    }
+	// 根据日志级别添加前缀
+	switch (LOG_LEVEL_GET(type))
+	{
+	case LOG_LEVEL_DEBUG:
+		snprintf(_buffer, sizeof(_buffer), "%10ld [DEBUG] ", xTaskGetTickCount());
+		break;
+	case LOG_LEVEL_INFO:
+		snprintf(_buffer, sizeof(_buffer), "%10ld [INFO ] ", xTaskGetTickCount());
+		break;
+	case LOG_LEVEL_WARNING:
+		snprintf(_buffer, sizeof(_buffer), "%10ld [WARN ] ", xTaskGetTickCount());
+		break;
+	case LOG_LEVEL_ERROR:
+		snprintf(_buffer, sizeof(_buffer), "%10ld [ERROR] ", xTaskGetTickCount());
+		break;
+	case LOG_LEVEL_FATAL:
+		snprintf(_buffer, sizeof(_buffer), "%10ld [FATAL] ", xTaskGetTickCount());
+		break;
+	default:
+		return; // 如果是未知的日志级别，直接返回
+	}
 
-    // 获取当前缓冲区的长度
-    int len = strlen(_buffer);
+	// 获取当前缓冲区的长度
+	int len = strlen(_buffer);
 
-    // 将格式化后的消息追加到缓冲区
-    va_list list{};
-    va_start(list, fmt);
-    vsnprintf(_buffer + len, sizeof(_buffer) - len, fmt, list);
-    va_end(list);
+	// 将格式化后的消息追加到缓冲区
+	va_list list{};
+	va_start(list, fmt);
+	vsnprintf(_buffer + len, sizeof(_buffer) - len, fmt, list);
+	va_end(list);
 
-    bsp::di::Console().WriteLine(_buffer);
+	bsp::di::Console().WriteLine(_buffer);
 }
